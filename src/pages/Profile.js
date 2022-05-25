6b92513da13a4ff328d44a597508754bb921afa8
Zubair Sheikh <izubairsh@gmail.com>
2022-05-26 01:56:37 +0500

    first commit

---
+++ src/pages/Profile.js
@@ -0,0 +1,39 @@
+import React, { Fragment, useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUser } from 'state/ducks/user/actions';
+import ProfileDescription from './components/ProfileDescription';
+
+const Profile = ({ history, match }) => {
+  const username = match.params.username;
+  const dispatch = useDispatch();
+  const { data: user } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    dispatch(getUser(username));
+  }, [username, dispatch]);
+
+  return (
+    <Fragment>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{username} - Chapy</title>
+      </Helmet>
+      <div
+        className="bg-black text-center"
+        style={{ padding: '10px 10px 10px 10px' }}
+      >
+        <a href="https://chapysocial.com" target="_blank" rel="noreferrer">
+          <img
+            src={process.env.PUBLIC_URL + '/logo.png'}
+            style={{ height: '40px' }}
+            alt=""
+          />
+        </a>
+      </div>
+      {user ? <ProfileDescription history={history} /> : <></>}
+    </Fragment>
+  );
+};
+
+export default Profile;
