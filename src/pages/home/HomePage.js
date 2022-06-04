8c1bc0b704b747fd112c85d746463a1dc353ad12
Zubair Sheikh <izubairsh@gmail.com>
2022-06-05 04:56:41 +0500

    added fortawesome

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -3,23 +3,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import MainLayout from 'components/MainLayout';
 import { Helmet } from 'react-helmet';
 import { getUser } from 'state/ducks/users/actions';
+import { Col, Row } from 'react-bootstrap';
+import Message from 'components/Message';
 
 const HomePage = ({ history }) => {
-  const [profile, setProfile] = useState(null);
   const { user: authUser } = useSelector((state) => state.auth);
-  const { user } = useSelector((state) => state.users);
+  const { user, error, profile } = useSelector((state) => state.users);
   const dispatch = useDispatch();
   useEffect(() => {
     if (!authUser) {
       history.push('/login');
-    } else if (!user) {
-      dispatch(getUser(authUser.username));
     } else {
-      if (user.isPersonal) {
-        setProfile(user.personal);
-      } else {
-        setProfile(user.business);
-      }
+      dispatch(getUser(authUser.username));
     }
   }, [history, authUser, user, dispatch]);
   return (
@@ -30,31 +25,37 @@ const HomePage = ({ history }) => {
             <meta charSet="utf-8" />
             <title>{authUser.username} - Info Card</title>
           </Helmet>
-
-          <div className="row">
-            <div className="col-md-6 m-auto text-center shadow p-3 mb-5 bg-white rounded">
-              <div className="">
-                {profile && profile.image && profile.image !== '' ? (
-                  <img
-                    src={process.env.REACT_APP_API_URL + profile.image}
-                    alt=""
-                    className="profile-image"
-                  />
+          <Row>
+            <Col md={4} />
+            <Col md={4}>
+              <div className="text-center">
+                {error ? <Message variant="danger">{error}</Message> : <></>}
+                {profile ? (
+                  <div className="">
+                    {profile.image && profile.image !== '' ? (
+                      <img
+                        src={process.env.REACT_APP_API_URL + profile.image}
+                        alt=""
+                        className="profile-image"
+                      />
+                    ) : (
+                      <img
+                        src={process.env.PUBLIC_URL + '/user.png'}
+                        alt=""
+                        className="profile-image"
+                      />
+                    )}
+                    <p>@{authUser.username ?? ''}</p>
+                    <h4>{profile.name ?? ''}</h4>
+                    <p>{profile.bio ?? ''}</p>
+                  </div>
                 ) : (
-                  <img
-                    src={process.env.PUBLIC_URL + '/user.png'}
-                    alt=""
-                    className="profile-image"
-                  />
+                  <></>
                 )}
               </div>
-              <h4>{profile ? profile.name : ''}</h4>
-              <p>@{user ? user.username : ''}</p>
-              <p style={{ wordWrap: 'break-word' }}>
-                {profile ? profile.bio : ''}
-              </p>
-            </div>
-          </div>
+            </Col>
+            <Col md={4} />
+          </Row>
         </Fragment>
       ) : (
         <></>
