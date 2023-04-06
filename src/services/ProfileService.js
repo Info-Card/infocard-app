0bc72cf69115edce0828928cf5c2f4b4dd035555
Yousaf Ramzan <yousaf.ramzan@devprotocols.com>
2023-04-06 11:24:34 +0500

    Refactor Profile service to improve readability and reduce complexity

---
+++ src/services/ProfileService.js
@@ -0,0 +1,85 @@
+import ApiService from "./ApiService";
+
+class ProfileService extends ApiService {
+  /**
+   * Update Profile with id and data
+   * @param {string} id * This is the id of profile
+   * @param {string} data * This is the data of new profile
+   */
+  updateProfile(id, data) {
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+    console.log(data);
+    const formData = new FormData();
+    Object.entries(data).forEach((entry) => {
+      const [key, value] = entry;
+      if (key === "image") {
+        if (value && value[0]) {
+          formData.append("image", value[0]);
+        }
+      } else {
+        formData.append(key, value);
+      }
+    });
+    return this.instance.patch(`profile/${id}`, formData, config);
+  }
+
+  /**
+   * Update tag using id with data
+   * @param {string} id * This is the id
+   * @param {string} data * This is the data
+   */
+  update(id, data) {
+    return this.instance.patch(`profile/${id}`, data);
+  }
+
+  /**
+   * Exchange with profileId and data
+   * @param {string} profileId * This is the ProfileId for exchange contact
+   * @param {string} data * This is the data for exchange contact
+   */
+  exchangeContact(profileId, data) {
+    return this.instance.post(`profile/exchange/${profileId}`, data);
+  }
+  /**
+   * Add custom Link with id and data
+   * @param {string} id * This is the id for add custom link
+   * @param {string} data * This is the data
+   */
+  addCustomLink(id, data) {
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+    console.log(data);
+    const formData = new FormData();
+    Object.entries(data).forEach((entry) => {
+      const [key, value] = entry;
+      if (key === "image") {
+        if (value && value[0]) {
+          formData.append("image", value[0]);
+        }
+      } else {
+        formData.append(key, value);
+      }
+    });
+    return this.instance.post(`profile/${id}/links/`, formData, config);
+  }
+
+  /**
+   * Delete custom Link with the ProfileId and id
+   * @param {string} profileId * This is the profile Id
+   * @param {string} id * This is the id for delete custom link
+   */
+  deleteCustomLink(profileId, id) {
+    return this.instance.delete(`profile/${profileId}/links/${id}`);
+  }
+}
+
+const profileService = new ProfileService();
+
+export default profileService;
