8d7b2e4daf1bf73b25386a06b077665f5a464801
Yousaf Ramzan <yousaf.ramzan@devprotocols.com>
2023-04-26 14:16:05 +0500

    Reset password page is Reverted

--- src/pages/auth/ResetPasswordPage.js
+++ src/pages/auth/ResetPasswordPage.js
@@ -7,34 +7,15 @@ import FormContainer from "components/FormContainer";
 import { resetPassword } from "state/ducks/auth/actions";
 import * as types from "state/ducks/auth/types";
 import { multilanguage } from "redux-multilanguage";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-
-const schema = yup.object().shape({
-  password: yup
-    .string()
-    .required("Password is required")
-    .min(8, "Password must be at least 8 characters"),
-  confirmPassword: yup
-    .string()
-    .oneOf([yup.ref("password"), null], "Passwords must match"),
-});
 
 const ResetPasswordPage = ({ location, history, strings }) => {
-  const dispatch = useDispatch();
   const query = new URLSearchParams(location.search);
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
+  const dispatch = useDispatch();
+
   const { success, error, loading } = useSelector((state) => state.auth);
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
 
   useEffect(() => {
     if (success) {
@@ -42,27 +23,18 @@ const ResetPasswordPage = ({ location, history, strings }) => {
       history.push("/login");
     }
   }, [history, success, dispatch]);
-  const onSubmit = (data) => {
-    console.log("ok on submit");
-    const token = query.get("token");
-    dispatch(resetPassword(token, password));
-    console.log(data);
-  };
 
   const submitHandler = (e) => {
     e.preventDefault();
     const token = query.get("token");
 
     if (confirmPassword === password) {
-      console.log("if is clear");
       dispatch(resetPassword(token, password));
     } else {
-      console.log("else is clear");
-      // <Message>"Password </Message>
-      // dispatch({
-      //   type: types.AUTH_FAIL,
-      //   payload: strings["Password doesnt match"],
-      // });
+      dispatch({
+        type: types.AUTH_FAIL,
+        payload: strings["Password doesnt match"],
+      });
     }
   };
 
@@ -76,27 +48,21 @@ const ResetPasswordPage = ({ location, history, strings }) => {
           <Form.Group controlId="password">
             <Form.Label>{strings["Password"]}</Form.Label>
             <Form.Control
-              name="password"
               type="password"
-              {...register("password")}
               placeholder={strings["Enter password"]}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             ></Form.Control>
           </Form.Group>
-          {errors.password && <p>{errors.password.message}</p>}
           <Form.Group controlId="confirmPassword">
             <Form.Label>{strings["Confirm Password"]}</Form.Label>
             <Form.Control
-              name="confirmPassword"
-              {...register("confirmPassword")}
               type="password"
               placeholder={strings["Enter confirm password"]}
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
             ></Form.Control>
           </Form.Group>
-          {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
           <Button type="submit" variant="primary">
             {strings["Update"]}
           </Button>
