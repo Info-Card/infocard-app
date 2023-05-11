b40bc1bdc198eb5d04a59c15b1cc41ce52164709
yousaf Ramzan <yousaf.ramzan@devprotocols.com>
2023-05-11 15:42:50 +0500

    Refactor in change password profile screen

--- src/pages/auth/ChangePasswordPage.js
+++ src/pages/auth/ChangePasswordPage.js
@@ -1,48 +1,18 @@
-import React, { useState, Fragment, useEffect } from "react";
-import { Form, Button } from "react-bootstrap";
+import React, { Fragment, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "components/Message";
 import Loader from "components/Loader";
 import FormContainer from "components/FormContainer";
 import * as types from "state/ducks/users/types";
 import MainLayout from "components/MainLayout";
-import { updateUser } from "state/ducks/users/actions";
 import { multilanguage } from "redux-multilanguage";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
+import ChangePasswordForm from "./components/ChangePasswordForm";
 
-const schema = yup.object().shape({
-  password: yup
-    .string()
-    .required("Password is required")
-    .max(
-      32,
-      "Error: Password is too long. Please enter a password with a maximum of 32 characters."
-    )
-    .min(8, "Password must be at least 8 characters"),
-  confirmPassword: yup
-    .string()
-    .oneOf([yup.ref("password"), null], "Passwords must match"),
-});
 const ChangePasswordPage = ({ location, history, strings }) => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-
   const dispatch = useDispatch();
-
   const { user: authUser } = useSelector((state) => state.auth);
   const { success, error, loading } = useSelector((state) => state.users);
-
   const { rehydrated } = useSelector((state) => state._persist);
-
   useEffect(() => {
     if (rehydrated) {
       if (!authUser) {
@@ -53,19 +23,6 @@ const ChangePasswordPage = ({ location, history, strings }) => {
       }
     }
   }, [history, success, dispatch, authUser, rehydrated]);
-
-  const onSubmit = (data) => {
-    if (confirmPassword === password) {
-      dispatch(updateUser({ password }));
-    } else {
-      dispatch({
-        type: types.USER_FAIL,
-        payload: strings["Password doesn't match"],
-      });
-    }
-    console.log(data);
-  };
-
   return (
     <MainLayout>
       <Fragment>
@@ -73,33 +30,7 @@ const ChangePasswordPage = ({ location, history, strings }) => {
           <h3>{strings["Change Password"]}</h3>
           {error && <Message variant="danger">{error}</Message>}
           {loading && <Loader />}
-          <Form onSubmit={handleSubmit(onSubmit)}>
-            <Form.Group controlId="password">
-              <Form.Label>{strings["Password"]}</Form.Label>
-              <Form.Control
-                type="password"
-                {...register("password")}
-                placeholder={strings["Enter password"]}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              ></Form.Control>
-            </Form.Group>
-            <p>{errors.password?.message}</p>
-            <Form.Group controlId="confirmPassword">
-              <Form.Label>{strings["Confirm Password"]}</Form.Label>
-              <Form.Control
-                type="password"
-                {...register("confirmPassword")}
-                placeholder={strings["Enter confirm password"]}
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-              ></Form.Control>
-            </Form.Group>
-            <p>{errors.confirmPassword?.message}</p>
-            <Button type="submit" variant="primary">
-              {strings["Update"]}
-            </Button>
-          </Form>
+          <ChangePasswordForm strings={strings} />
 
           {/* <Row className="py-3">
         <Col>
