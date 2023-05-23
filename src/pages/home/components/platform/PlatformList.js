ebd83b72fe5a88d198cf498e7cd0e3ee70e1d63a
yousaf Ramzan <yousaf.ramzan@devprotocols.com>
2023-05-23 18:14:50 +0500

    Add by default card to navigate profile page

--- src/pages/home/components/platform/PlatformList.js
+++ src/pages/home/components/platform/PlatformList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import PlatformCard from "./PlatformCard";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const PlatformList = ({ platforms, direct }) => {
   return (
@@ -17,6 +18,18 @@ const PlatformList = ({ platforms, direct }) => {
                   </Col>
                 );
               })}
+            </Row>
+            <Row className="g-2">
+              <Col xs={12}>
+                <div className="platform-card p-2 mt-2 text-center">
+                  <p className="m-0">
+                    {["+"]}{" "}
+                    <Link to={"/profile"}>
+                      {["Add Links and Contect Info"]}
+                    </Link>{" "}
+                  </p>
+                </div>
+              </Col>
             </Row>
           </Col>
         </>
