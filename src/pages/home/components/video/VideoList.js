658323884094ef308b0a739c97dd101a35471b76
yousaf Ramzan <yousaf.ramzan@devprotocols.com>
2023-05-24 18:04:51 +0500

    Change settings on video swipper and change in upload profile photo

--- src/pages/home/components/video/VideoList.js
+++ src/pages/home/components/video/VideoList.js
@@ -16,11 +16,35 @@ const VideoList = ({ videos }) => {
       <h5>Videos</h5>
       <Swiper
         slidesPerView={1}
-        pagination={{ clickable: true }}
+        pagination={{ clickable: true, dynamicBullets: true }}
+        on={{
+          slideChangeTransitionStart: () => {
+            const videos = document.querySelectorAll("video");
+            Array.prototype.forEach.call(videos, function (video) {
+              video.pause();
+            });
+          },
+          slideChangeTransitionEnd: () => {
+            const activeIndex = this.swiper.activeIndex;
+            const activeSlide =
+              document.getElementsByClassName("swiper-slide")[activeIndex];
+            const activeSlideVideo =
+              activeSlide.getElementsByTagName("video")[0];
+            activeSlideVideo.play();
+          },
+        }}
         loop
         spaceBetween={26}
         touchEventsTarget="container"
       >
+        {/* <Swiper
+        slidesPerView={1}
+        pagination={{ clickable: true, dynamicBullets: true }}
+        modules={[Pagination]}
+        loop
+        spaceBetween={26}
+        touchEventsTarget="container"
+      > */}
         {videos.map((video) => {
           return (
             <SwiperSlide key={video}>
