6f2ed8580727b20632afa5b3169f86c72a11b92e
yousaf Ramzan <yousaf.ramzan@devprotocols.com>
2023-08-15 15:57:40 +0500

    fix logo margin and platform margin on profile details screen

--- src/pages/profile/components/profile/components/ProfileDetailsHeader.js
+++ src/pages/profile/components/profile/components/ProfileDetailsHeader.js
@@ -4,7 +4,21 @@ import { Button } from "react-bootstrap";
 const ProfileDetailsHeader = () => {
   return (
     <>
-      <div className="d-flex align-items-center justify-content-between  mt-3 mb-1">
+      <style>
+        {`
+          @media (max-width: 575px) {
+            .header-container img, .header-container a {
+              margin-left: 15px; /* Add margin to both sides */
+            }
+          }
+          @media (max-width: 575px) {
+            .header-container a {
+              margin-right: 15px; /* Add margin to both sides */
+            }
+          }
+        `}
+      </style>
+      <div className="d-flex align-items-center justify-content-between mt-3 mb-1 header-container">
         <img src="assets/images/logo.png" alt="" style={{ width: "80px" }} />
         <a href={`https://infocard.me`}>
           <Button type="submit" variant="outline-primary">
