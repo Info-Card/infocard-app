deaedf8d2def74385bf151d5c0aceeec862382ac
yousaf Ramzan <yousaf.ramzan@devprotocols.com>
2023-08-17 18:54:20 +0500

    Fix missing character limit validation on linked card name field

--- src/pages/profile/components/UpdateCardModal.js
+++ src/pages/profile/components/UpdateCardModal.js
@@ -3,13 +3,15 @@ import { Button, Form, Modal } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { multilanguage } from "redux-multilanguage";
 import { updateTag } from "state/ducks/tags/actions";
+import { useForm } from "react-hook-form";
 
 const UpdateCardModal = ({ strings, tag, setTag }) => {
   const dispatch = useDispatch();
   const [tagName, setTagName] = useState(tag?.name);
-  const handleUpdateTag = (event) => {
-    event.preventDefault();
-    dispatch(updateTag(tag.id, { name: tagName }));
+  const { handleSubmit, register, errors } = useForm();
+
+  const handleUpdateTag = (data) => {
+    dispatch(updateTag(tag.id, { name: data.name }));
     setTag(null);
   };
 
@@ -20,15 +22,22 @@ const UpdateCardModal = ({ strings, tag, setTag }) => {
           <Modal.Title>{strings["Update Tag information"]}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form onSubmit={handleUpdateTag}>
+          <Form onSubmit={handleSubmit(handleUpdateTag)}>
             <Form.Group controlId="name">
               <Form.Label>{strings["Tag Name"]}</Form.Label>
               <Form.Control
-                type="name"
+                type="text"
+                name="name"
                 placeholder={strings["Enter name"]}
                 value={tagName}
                 onChange={(e) => setTagName(e.target.value)}
-              ></Form.Control>
+                ref={register({ required: true, maxLength: 15 })}
+              />
+              {errors.name && (
+                <div className="text-danger">
+                  Tag name is required and must be at most 15 characters.
+                </div>
+              )}
             </Form.Group>
             <Button type="submit" variant="primary">
               {strings["Update"]}
