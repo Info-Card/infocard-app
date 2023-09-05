e46bec9e7265b80196c42badcc836fdf310b1237
Zubair Sheikh <izubairsh@gmail.com>
2023-09-05 13:23:52 +0500

    fix: image cropper issue

--- src/pages/profile/components/ImageOptionsModal.js
+++ src/pages/profile/components/ImageOptionsModal.js
@@ -10,6 +10,7 @@ const ImageOptionsModal = ({ show, setShow }) => {
   const { profile } = useSelector((state) => state.users);
   const [imageSrc, setImageSrc] = useState(null);
   const [croppedImage, setCroppedImage] = useState(null);
+
   useEffect(() => {
     if (croppedImage) {
       console.log(croppedImage);
@@ -19,10 +20,13 @@ const ImageOptionsModal = ({ show, setShow }) => {
       setCroppedImage(null);
     }
   }, [dispatch, croppedImage, profile.id, setShow]);
+
   const selectImage = async () => {
     const inputElement = document.createElement("input");
     inputElement.type = "file";
+    document.body.appendChild(inputElement);
     inputElement.onchange = async (event) => {
+      console.log(event);
       const file = event.target.files[0];
       if (file.type === "image/heic" || file.type === "image/heif") {
         const convertedBlob = await heic2any({
@@ -41,9 +45,12 @@ const ImageOptionsModal = ({ show, setShow }) => {
         };
         reader.readAsDataURL(file);
       }
+      document.body.removeChild(inputElement);
     };
+    console.log("clicked");
     inputElement.click();
   };
+
   const deleteImage = () => {
     setShow(false);
     dispatch(updateProfile(profile.id, { image: "" }));
