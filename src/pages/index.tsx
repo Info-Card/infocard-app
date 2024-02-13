0320831badd6a3008390623ee3680b1e4ec8b0d7
Zubair Sheikh <zubair@xertk.com>
2024-02-13 05:55:08 +0500

    feat(icons): update home screen icons for consistency

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,6 +20,7 @@ import { useRouter } from 'next/router';
 import { useLazyLinkTagQuery } from '@/store/tag';
 import { showAlert } from '@/utils/show-alert';
 import { useGetLinksQuery } from '@/store/link';
+import { FaInfoCircle } from 'react-icons/fa';
 
 const HomePage = () => {
   const router = useRouter();
@@ -212,24 +213,11 @@ const HomePage = () => {
             <VideosList profile={user.live} refetch={refetch} />
             <ProductsList profile={user.live} />
             <div className="d-flex justify-content-between mx-2">
-              <div className="d-flex ">
-                <i
-                  className="mx-2 bi bi-info-circle "
+              <div className="d-flex align-items-center">
+                <FaInfoCircle
+                  size={20}
                   onClick={() => handleInfoIconClick('direct')}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="22"
-                    height="22"
-                    fill="currentColor"
-                    className="bi bi-info-circle"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                    <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
-                  </svg>
-                </i>
-
+                />
                 <CustomToggle
                   id="direct"
                   checked={user.live?.isDirect}
@@ -237,23 +225,11 @@ const HomePage = () => {
                   label="Direct"
                 />
               </div>
-              <div className="d-flex">
-                <i
-                  className="mx-2 bi bi-info-circle "
+              <div className="d-flex align-items-center">
+                <FaInfoCircle
+                  size={20}
                   onClick={() => handleInfoIconClick('private')}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="22"
-                    height="22"
-                    fill="currentColor"
-                    className="bi bi-info-circle"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                    <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
-                  </svg>
-                </i>
+                />
                 <CustomToggle
                   id="private"
                   checked={user.live?.isPrivate}
